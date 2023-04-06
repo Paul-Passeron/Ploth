0ba2b292b368120ef25905a38dce798f85863120
Paul-Passeron <paul.passeron2@gmail.com>
2023-04-06 12:52:41 +0200

    Changed data structures

--- main.ml
+++ main.ml
@@ -6,6 +6,11 @@ type intrinsic =
   | Drop
   | Over;;
 
+type keyword = 
+  | If
+  | Else
+  | End;;
+
 type arithmetic =
   | Plus
   | Mult
@@ -27,10 +32,12 @@ type gate =
   | Not;;
 
 type prog_token = 
+  | NoOp
   | Intrinsic of intrinsic
   | Arithmetic of arithmetic
   | Comparator of comparator
   | Gate of gate
+  | Keyword of keyword
   | Int of int
   | Bool of bool;;
 
@@ -67,8 +74,19 @@ let push (elem: 'a) (s: 'a stack) :'a stack = Node(elem, s);;
 let pop (s: 'a stack): 'a * 'a stack = match s with
   | Empty -> failwith "Can't pop an empty stack."
   | Node(a,s) -> a, s;;
+(*
+type sub_prog =
+  | Op of prog_token
+  | Branch of bool * (prog_token list);;
 
-type prog = prog_token list;;
+type prog = sub_prog list;;
+*)
+
+type 'a prog_elem =
+  | Op of prog_token
+  | Branch of ('a * 'a prog)
+and 'a prog = ('a prog_elem) list;;
+
 
 let print_bool b = match b with
   | true -> print_string "true"
@@ -76,7 +94,6 @@ let print_bool b = match b with
 
 let compiler_message_error_exp (expected: prog_token) (actual: prog_token) =
   "ERROR: Expected "^(human expected)^" but actually got "^(human actual);;
-
 
 let ar_plus (s: prog_token stack): prog_token stack =
   let cons_b, s1 = pop s  in
@@ -122,7 +139,6 @@ let ar_max (s: prog_token stack): prog_token stack =
     | _, Int(_) -> failwith (compiler_message_error_exp (Int(0)) cons_b)
     | Int(_), _ -> failwith (compiler_message_error_exp (Int(0)) cons_a)
     | _, _ -> failwith (compiler_message_error_exp (Int(0)) cons_b);;
-
 
 let in_drop (s: prog_token stack): prog_token stack = let _, s1 = pop s in s1;;
 
@@ -211,39 +227,38 @@ let co_neq (s: prog_token stack): prog_token stack =
     | Bool(b), Bool(a) -> push (Bool(a <> b)) s2
     | _, _ -> failwith (compiler_message_error_exp cons_a cons_b);;
 
-
-let parse_prog (p: prog): prog_token stack = 
+let parse_prog (p: 'a prog): prog_token stack = 
   let rec aux p acc = match p with
     | [] -> acc
-    | Arithmetic(Plus)::q -> aux q (ar_plus acc)
-    | Arithmetic(Minus)::q -> aux q (ar_minus acc)
-    | Arithmetic(Mult)::q -> aux q (ar_mult acc)
-    | Arithmetic(Divmod)::q -> aux q (ar_divmod acc)
-    | Arithmetic(Max)::q -> aux q (ar_max acc)
-    | Intrinsic(Drop)::q -> aux q (in_drop acc)
-    | Intrinsic(Print)::q -> aux q (in_print acc)
-    | Intrinsic(Dup)::q -> aux q (in_dup acc)
-    | Intrinsic(Swap)::q -> aux q (in_swap acc)
-    | Intrinsic(Over)::q -> aux q (in_over acc)
-    | Intrinsic(Rot)::q -> aux q (in_rot acc)
-    | Comparator(Geq)::q -> aux q (co_geq acc)
-    | Comparator(G)::q -> aux q (co_g acc)
-    | Comparator(Leq)::q -> aux q (co_leq acc)
-    | Comparator(L)::q -> aux q (co_l acc)
-    | Comparator(Eq)::q -> aux q (co_eq acc)
-    | Comparator(Neq)::q -> aux q (co_neq acc)
-    | Gate(Or)::q -> (
+    | Op(Arithmetic(Plus))::q -> aux q (ar_plus acc)
+    | Op(Arithmetic(Minus))::q -> aux q (ar_minus acc)
+    | Op(Arithmetic(Mult))::q -> aux q (ar_mult acc)
+    | Op(Arithmetic(Divmod))::q -> aux q (ar_divmod acc)
+    | Op(Arithmetic(Max))::q -> aux q (ar_max acc)
+    | Op(Intrinsic(Drop))::q -> aux q (in_drop acc)
+    | Op(Intrinsic(Print))::q -> aux q (in_print acc)
+    | Op(Intrinsic(Dup))::q -> aux q (in_dup acc)
+    | Op(Intrinsic(Swap))::q -> aux q (in_swap acc)
+    | Op(Intrinsic(Over))::q -> aux q (in_over acc)
+    | Op(Intrinsic(Rot))::q -> aux q (in_rot acc)
+    | Op(Comparator(Geq))::q -> aux q (co_geq acc)
+    | Op(Comparator(G))::q -> aux q (co_g acc)
+    | Op(Comparator(Leq))::q -> aux q (co_leq acc)
+    | Op(Comparator(L))::q -> aux q (co_l acc)
+    | Op(Comparator(Eq))::q -> aux q (co_eq acc)
+    | Op(Comparator(Neq))::q -> aux q (co_neq acc)
+    | Op(Gate(Or))::q -> (
       let Bool(b), s1 = pop acc in
       let Bool(a), s2 = pop s1 in
       aux q (push (Bool(a||b)) s2))
-    | Gate(And)::q -> (
+    | Op(Gate(And))::q -> (
       let Bool(b), s1 = pop acc in
       let Bool(a), s2 = pop s1 in
       aux q (push (Bool(a&&b)) s2))
-    | Gate(Not)::q -> (
+    | Op(Gate(Not))::q -> (
       let Bool(a), s1 = pop acc in
       aux q (push (Bool(not a)) s1))
-    | e::q -> aux q (push e acc) in
+    | Op(e)::q -> aux q (push e acc) in
   aux p Empty;;
 
 let splice_line (s: string): string list =
@@ -261,38 +276,51 @@ let get_prog_from_file (filename: string): string list =
   with End_of_file -> close_in ic;
   !res;;
 
-let rec get_prog_from_l_o_t (l: string list): prog = match l with
-    | [] -> []
-    | "+"::q -> Arithmetic(Plus)::(get_prog_from_l_o_t q)
-    | "-"::q -> Arithmetic(Minus)::(get_prog_from_l_o_t q)
-    | "*"::q -> Arithmetic(Mult)::(get_prog_from_l_o_t q)
-    | "divmod"::q -> Arithmetic(Divmod)::(get_prog_from_l_o_t q)
-    | "max"::q -> Arithmetic(Max)::(get_prog_from_l_o_t q)
-    | "print"::q -> Intrinsic(Print)::(get_prog_from_l_o_t q)
-    | "dup"::q -> Intrinsic(Dup)::(get_prog_from_l_o_t q)
-    | "swap"::q -> Intrinsic(Swap)::(get_prog_from_l_o_t q)
-    | "drop"::q -> Intrinsic(Drop)::(get_prog_from_l_o_t q)
-    | "rot"::q -> Intrinsic(Rot)::(get_prog_from_l_o_t q)
-    | "over"::q -> Intrinsic(Over)::(get_prog_from_l_o_t q)
-    | "true"::q -> Bool(true)::(get_prog_from_l_o_t q)
-    | "false"::q -> Bool(false)::(get_prog_from_l_o_t q)
-    | "<="::q -> Comparator(Leq)::(get_prog_from_l_o_t q)
-    | "<"::q -> Comparator(L)::(get_prog_from_l_o_t q)
-    | ">="::q -> Comparator(Geq)::(get_prog_from_l_o_t q)
-    | ">"::q -> Comparator(G)::(get_prog_from_l_o_t q)
-    | "="::q -> Comparator(Eq)::(get_prog_from_l_o_t q)
-    | "!="::q -> Comparator(Neq)::(get_prog_from_l_o_t q)
-    | "!!"::q -> Gate(Not)::(get_prog_from_l_o_t q)
-    | "&&"::q -> Gate(And)::(get_prog_from_l_o_t q)
-    | "||"::q -> Gate(Or)::(get_prog_from_l_o_t q)
-    | e::q -> Int(int_of_string e)::(get_prog_from_l_o_t q);;
 
+
+let get_tok_l (l: string list): prog_token list =
+  let rec aux l = match l with
+    | [] -> []
+    | "+"::q -> Arithmetic(Plus)::(aux q )
+    | "-"::q -> Arithmetic(Minus)::(aux q )
+    | "*"::q -> Arithmetic(Mult)::(aux q )
+    | "divmod"::q -> Arithmetic(Divmod)::(aux q )
+    | "max"::q -> Arithmetic(Max)::(aux q )
+    | "print"::q -> Intrinsic(Print)::(aux q )
+    | "dup"::q -> Intrinsic(Dup)::(aux q )
+    | "swap"::q -> Intrinsic(Swap)::(aux q )
+    | "drop"::q -> Intrinsic(Drop)::(aux q )
+    | "rot"::q -> Intrinsic(Rot)::(aux q )
+    | "over"::q -> Intrinsic(Over)::(aux q )
+    | "true"::q -> Bool(true)::(aux q )
+    | "false"::q -> Bool(false)::(aux q )
+    | "<="::q -> Comparator(Leq)::(aux q )
+    | "<"::q -> Comparator(L)::(aux q )
+    | ">="::q -> Comparator(Geq)::(aux q )
+    | ">"::q -> Comparator(G)::(aux q )
+    | "="::q -> Comparator(Eq)::(aux q )
+    | "!="::q -> Comparator(Neq)::(aux q )
+    | "!!"::q -> Gate(Not)::(aux q )
+    | "&&"::q -> Gate(And)::(aux q )
+    | "||"::q -> Gate(Or)::(aux q )
+    | "if"::q -> Keyword(If)::(aux q)
+    | "else"::q -> Keyword(Else)::(aux q)
+    | "end"::q -> Keyword(End)::(aux q)
+    | e::q -> Int(int_of_string e)::(aux q ) in
+  aux l;;
+(*
 let run_programme p = let _ = parse_prog p in ();;
 
 let run_programme_from_file (filename: string) = 
   let programme = get_prog_from_file filename in
   let p =  get_prog_from_l_o_t programme in
-  parse_prog p;;
-
+  parse_prog p;;*)
+(*
 run_programme_from_file "test_prog.txt";;
 get_prog_from_file "test_prog.txt";;
+*)
+
+
+
+let string_l = get_prog_from_file "test_prog.txt"
+let tok_l = get_tok_l string_l;;
